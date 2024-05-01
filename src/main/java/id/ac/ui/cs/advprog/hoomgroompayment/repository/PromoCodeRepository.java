4d917a1180d244fe6011dc186e9e2c3f0d86d270
Lidwina Eurora <lidwinaeurora@Lidwinas-MacBook-Air.local>
2024-05-01 13:12:39 +0700

    [GREEN] Implement PromoCodeRepository class.

--- src/main/java/id/ac/ui/cs/advprog/hoomgroompayment/repository/PromoCodeRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroompayment/repository/PromoCodeRepository.java
@@ -9,11 +9,35 @@ import id.ac.ui.cs.advprog.hoomgroompayment.model.PromoCode;
 public class PromoCodeRepository {
     private List<PromoCode> promoCodeData = new ArrayList<>();
 
-    public PromoCode save(PromoCode promoCode) {return null;}
+    public PromoCode save(PromoCode promoCode) {
+        for (PromoCode savedPromo : promoCodeData) {
+            if (savedPromo.getId().equals(promoCode.getId())) {
+                promoCodeData.remove(savedPromo);
+                break;
+            }
+        }
+        promoCodeData.add(promoCode);
+        return promoCode;
+    }
 
-    public PromoCode findById(String id) {return null;}
+    public PromoCode findById(String id) {
+        for (PromoCode savedPromo : promoCodeData) {
+            if (id.equals(savedPromo.getId().toString())) {
+                return savedPromo;
+            }
+        }
+        return null;
+    }
 
-    public PromoCode deleteById(String id) {return null;}
+    public PromoCode deleteById(String id) {
+        PromoCode promoCode = findById(id);
+        if (promoCode != null) {
+            promoCodeData.remove(promoCode);
+        }
+        return promoCode;
+    }
 
-    public Iterator<PromoCode> findAll() {return null;}
+    public Iterator<PromoCode> findAll() {
+        return promoCodeData.iterator();
+    }
 }
