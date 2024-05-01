4db6d06d05709c632824efc81b871647b72f111c
VirgilliaYeala <virgillia.yeala@ui.ac.id>
2024-05-01 17:50:46 +0700

    [GREEN] Implement TopUpRepository class

--- src/main/java/id/ac/ui/cs/advprog/hoomgroompayment/model/TopUp.java
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroompayment/model/TopUp.java
@@ -1,41 +1,42 @@
 package id.ac.ui.cs.advprog.hoomgroompayment.model;
 
+import id.ac.ui.cs.advprog.hoomgroompayment.enums.TopUpAmount;
+import id.ac.ui.cs.advprog.hoomgroompayment.model.primary.keys.TopUpPrimaryKeys;
 import lombok.Getter;
-import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import jakarta.persistence.*;
 import java.util.Date;
-import id.ac.ui.cs.advprog.hoomgroompayment.enums.TopUpAmount;
 
 @Entity
 @Table(name = "topups-payment")
-@Getter @Setter
+@IdClass(TopUpPrimaryKeys.class)
+@NoArgsConstructor
+@Getter
+@Setter
 public class TopUp {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     private String username;
-    private String topUpMethod;
+
+    private String topUpMethod = "dummy wallet";
+
+    @Id
     private Date timestamp;
+
     private double amount;
 
     public TopUp(Date timestamp, String username, double amount) {
         this.timestamp = timestamp;
         this.username = username;
-        this.topUpMethod = "dummy wallet";
         setAmount(amount);
-    }
-
-    public TopUp() {
-        this.topUpMethod = "dummy wallet";
     }
 
     public void setAmount(double amount) {
         if (TopUpAmount.contains(amount)) {
             this.amount = amount;
         } else {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Invalid top-up amount");
         }
     }
 }
