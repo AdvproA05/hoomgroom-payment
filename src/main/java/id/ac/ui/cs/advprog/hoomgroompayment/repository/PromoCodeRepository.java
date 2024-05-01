e3ebe1a7d4bd0104759355fc913cb3c0f3c07c35
Lidwina Eurora <lidwinaeurora@Lidwinas-MacBook-Air.local>
2024-05-01 13:00:33 +0700

    [RED] Add PromoCodeRepository skeleton.

---
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroompayment/repository/PromoCodeRepository.java
@@ -0,0 +1,19 @@
+package id.ac.ui.cs.advprog.hoomgroompayment.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
+import id.ac.ui.cs.advprog.hoomgroompayment.model.PromoCode;
+
+public class PromoCodeRepository {
+    private List<PromoCode> promoCodeData = new ArrayList<>();
+
+    public PromoCode save(PromoCode promoCode) {return null;}
+
+    public PromoCode findById(String id) {return null;}
+
+    public PromoCode deleteById(String id) {return null;}
+
+    public Iterator<PromoCode> findAll() {return null;}
+}
