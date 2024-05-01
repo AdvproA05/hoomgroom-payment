3c39d475de98bf7458025d987a05c8624912c6bf
VirgilliaYeala <virgillia.yeala@ui.ac.id>
2024-05-01 15:30:24 +0700

    [REFACTOR] Apply TopUpAmount enum to TopUp model tests

--- src/test/java/id/ac/ui/cs/advprog/hoomgroompayment/model/TopUpTest.java
+++ src/test/java/id/ac/ui/cs/advprog/hoomgroompayment/model/TopUpTest.java
@@ -5,13 +5,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Date;
 
+import id.ac.ui.cs.advprog.hoomgroompayment.enums.TopUpAmount;
+
 public class TopUpTest {
 
     @Test
     public void testTopUpConstructor() {
         Date timestamp = new Date();
         String username = "testUser";
-        int amount = 10000;
+        double amount = TopUpAmount.TENRUPIAH.getValue();
 
         TopUp topUp = new TopUp(timestamp, username, amount);
 
@@ -25,7 +27,7 @@ public class TopUpTest {
     public void testTopUpSetter() {
         Date timestamp = new Date();
         String username = "testUser";
-        int amount = 25000;
+        double amount = TopUpAmount.TWENTYFIVERUPIAH.getValue();
 
         TopUp topUp = new TopUp();
         topUp.setTimestamp(timestamp);
@@ -42,7 +44,7 @@ public class TopUpTest {
     public void testTopUpEquals() {
         Date timestamp = new Date();
         String username = "testUser";
-        int amount = 50000;
+        double amount = TopUpAmount.FIFTYRUPIAH.getValue();
 
         TopUp topUp1 = new TopUp(timestamp, username, amount);
         TopUp topUp2 = new TopUp(timestamp, username, amount);
@@ -55,8 +57,8 @@ public class TopUpTest {
         Date timestamp1 = new Date();
         Date timestamp2 = new Date(timestamp1.getTime() + 1000); // Different timestamp
         String username = "testUser";
-        int amount1 = 100000;
-        int amount2 = 25000;
+        double amount1 = TopUpAmount.ONEHUNDREDRUPIAH.getValue();
+        double amount2 = TopUpAmount.TENRUPIAH.getValue();
 
         TopUp topUp1 = new TopUp(timestamp1, username, amount1);
         TopUp topUp2 = new TopUp(timestamp2, username, amount2);
@@ -69,7 +71,7 @@ public class TopUpTest {
         Date timestamp = new Date();
         String username1 = "testUser1";
         String username2 = "testUser2";
-        int amount = 50000;
+        double amount = TopUpAmount.FIFTYRUPIAH.getValue();
 
         TopUp topUp1 = new TopUp(timestamp, username1, amount);
         TopUp topUp2 = new TopUp(timestamp, username2, amount);
@@ -82,7 +84,7 @@ public class TopUpTest {
         Date timestamp1 = new Date();
         Date timestamp2 = new Date(timestamp1.getTime() + 1000);
         String username = "testUser";
-        int amount = 50000;
+        double amount = TopUpAmount.FIFTYRUPIAH.getValue();
 
         TopUp topUp1 = new TopUp(timestamp1, username, amount);
         TopUp topUp2 = new TopUp(timestamp2, username, amount);
