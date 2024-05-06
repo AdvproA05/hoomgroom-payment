f24065f2e90cd10588f92fbda7bcc45c67d8340d
VirgilliaYeala <virgillia.yeala@ui.ac.id>
2024-05-06 16:36:49 +0700

    [GREEN] Implement UserDetailsRepository class

--- src/main/java/id/ac/ui/cs/advprog/hoomgroompayment/repository/UserDetailsRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroompayment/repository/UserDetailsRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import id.ac.ui.cs.advprog.hoomgroompayment.model.UserDetails;
 
 public interface UserDetailsRepository extends JpaRepository<UserDetails, Long> {
+    UserDetails findByUsername(String username);
 }
