6aa232d658f7dc21568f41702d3de5ebe4a9480f
VirgilliaYeala <virgillia.yeala@ui.ac.id>
2024-05-06 16:30:59 +0700

    [GREEN] Add JWT service for authentication and authorization

---
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroompayment/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,35 @@
+package id.ac.ui.cs.advprog.hoomgroompayment.security.jwt;
+
+import java.io.IOException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Service
+public class AuthTokenFilter {
+    private JwtUtils jwtUtils;
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7, headerAuth.length());
+        }
+
+        return null;
+    }
+}
