29d18f786923ce373e8e652f9a679cac9862115f
Lidwina Eurora <lidwinaeurora@Lidwinas-MacBook-Air.local>
2024-05-15 20:42:04 +0700

    Update application properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,13 +24,13 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-  implementation("org.springframework.boot:spring-boot-starter-web")
+  	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation ("org.postgresql:postgresql")
 	compileOnly("org.projectlombok:lombok")
-  developmentOnly("org.springframework.boot:spring-boot-devtools")
-  annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-  annotationProcessor("org.projectlombok:lombok")
-  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  	developmentOnly("org.springframework.boot:spring-boot-devtools")
+  	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+  	annotationProcessor("org.projectlombok:lombok")
+  	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -38,6 +38,7 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-api:0.11.2")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
+	runtimeOnly("org.postgresql:postgresql")
 }
 
 tasks.register<Test>("unitTest") {
